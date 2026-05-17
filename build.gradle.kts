e8116a9e22579f0ea57ff4b4c5774886663fa721
Sunghyouk Bae <sunghyouk.bae@gmail.com>
2026-05-17 12:52:30 +0900

    fix: disable Spring DM withXml POM customization to fix Gradle 9.5 NPE (#133)
    generatedPomCustomization { setEnabled(false) } prevents the
    io.spring.dependency-management plugin 1.1.7 withXml action from calling
    ConfigurationContainer.detachedConfiguration() when delegate is null,
    which causes a NullPointerException during generatePomFile with Gradle 9.5+.

--- build.gradle.kts
+++ build.gradle.kts
@@ -227,6 +227,7 @@ subprojects {
 
     dependencyManagement {
         setApplyMavenExclusions(false)
+        generatedPomCustomization { setEnabled(false) }
         imports {
             mavenBom(rootLibs.bluetape4k.bom.get().toString())
             mavenBom(rootLibs.bluetape4k.dependencies.get().toString())
