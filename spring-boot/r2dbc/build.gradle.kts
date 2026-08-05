bluetape4k/bluetape4k-projects

Move shared dependency accessors under central catalog authority
Constraint: The repository must consume governed library, plugin, and version aliases from the pinned bt4k catalog while retaining repo-local structural settings plugins.\nRejected: Keep duplicate versioned local aliases | that preserves competing version authorities.\nConfidence: high\nScope-risk: broad\nDirective: Validate with an explicit candidate catalog until an immutable catalog tag updates the default ref.\nTested: git diff --check; BLUETAPE4K_DEPENDENCIES_CATALOG_PATH=<central-candidate> ./gradlew --offline --no-daemon help\nNot-tested: Full build, publication POMs, and default-ref CI pending immutable catalog tag

spring-boot/r2dbc/build.gradle.kts
@@ -16,9 +16,9 @@ dependencies {
 
     // R2DBC
     api("org.springframework.boot:spring-boot-starter-data-r2dbc")
-    testImplementation(libs.r2dbc.pool)
+    testImplementation(bt4k.r2dbc.pool)
     testImplementation(bt4k.r2dbc.h2)
-    testRuntimeOnly(libs.h2.v2)
+    testRuntimeOnly(bt4k.h2.v2)
 
     // Jackson 3
     testImplementation(project(":bluetape4k-jackson3"))
@@ -33,7 +33,7 @@ dependencies {
     // PostgreSql Server
     // testImplementation(project(":bluetape4k-testcontainers"))
     // testImplementation(libs.testcontainers.postgresql)
-    // testImplementation(libs.r2dbc.postgresql)
+    // testImplementation(bt4k.r2dbc.postgresql)
 
     // Spring Boot for Blog Application
     testImplementation("org.springframework.boot:spring-boot-starter-webflux")

bluetape4k/bluetape4k-exposed

spring-boot/r2dbc/build.gradle.kts
@@ -43,19 +43,19 @@ dependencies {
     compileOnly(project(":bluetape4k-exposed-r2dbc-caffeine"))
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation(libs.mockk)
+    testImplementation(bt4k.mockk)
 
-    testImplementation(libs.h2.v2)
+    testImplementation(bt4k.h2.v2)
     testImplementation(bt4k.r2dbc.h2)
     testImplementation(bt4k.hikaricp)
 
     // Multi-DB 테스트용 R2DBC 드라이버
-    testImplementation(libs.r2dbc.mysql)
-    testImplementation(libs.r2dbc.mariadb)
-    testImplementation(libs.r2dbc.postgresql)
+    testImplementation(bt4k.r2dbc.mysql)
+    testImplementation(bt4k.r2dbc.mariadb)
+    testImplementation(bt4k.r2dbc.postgresql)
 
     // Multi-DB 테스트용 JDBC 드라이버 (Testcontainers 컨테이너 연결용)
     testImplementation(bt4k.mysql.connector.j)
-    testImplementation(libs.mariadb.java.client)
+    testImplementation(bt4k.mariadb.java.client)
     testImplementation(bt4k.postgresql)
 }